9d743bd97071b09463ef568deb42ba189d82c569
Rikharthu <uberviolence@gmail.com>
2017-02-12 13:23:38 +0200

    minor adds

---
+++ app/src/main/java/com/rtu/uberv/divinote/EditNoteActivity.java
@@ -0,0 +1,31 @@
+package com.rtu.uberv.divinote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class EditNoteActivity extends AppCompatActivity {
+    public static final String LOG_TAG=EditNoteActivity.class.getSimpleName();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+    }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
