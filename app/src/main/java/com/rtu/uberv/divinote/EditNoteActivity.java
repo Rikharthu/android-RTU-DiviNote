5117b4949ee86e3b221f78458c57acfb97e355dd
Rikharthu <uberviolence@gmail.com>
2017-02-16 08:31:15 +0200

    something i don't remember

--- app/src/main/java/com/rtu/uberv/divinote/EditNoteActivity.java
+++ app/src/main/java/com/rtu/uberv/divinote/EditNoteActivity.java
@@ -1,11 +1,18 @@
 package com.rtu.uberv.divinote;
 
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
 
+import com.rtu.uberv.divinote.fragments.DatePickerFragment;
+import com.rtu.uberv.divinote.fragments.TimePickerFragment;
 import com.rtu.uberv.divinote.models.Note;
+
+import java.util.Calendar;
 
 public class EditNoteActivity extends AppCompatActivity {
     // TODO activity flags
@@ -14,12 +21,23 @@ public class EditNoteActivity extends AppCompatActivity {
     public static final String KEY_NOTE="KEY_NOTE";
     public static final String KEY_SAVED_NOTE="KEY_SAVED_NOTE";
 
+    // Views
+    ImageButton pickDateIb;
+    // Fields
     private Note note;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_note);
+
+        pickDateIb = (ImageButton) findViewById(R.id.pickDateIb);
+        pickDateIb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onPickDate();
+            }
+        });
 
         // TODO different actions depending on intent arguments
         if(savedInstanceState==null){
@@ -33,7 +51,15 @@ public class EditNoteActivity extends AppCompatActivity {
             // no Note saved nor we did not receive any to edit
             // Create a new Note
             note = new Note();
-            note.setCreatedAt(System.currentTimeMillis());
+            long createdAtMillis = System.currentTimeMillis();
+            note.setCreatedAt(createdAtMillis);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(createdAtMillis);
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
         }
 
         // Find the toolbar view inside the activity layout
@@ -41,6 +67,11 @@ public class EditNoteActivity extends AppCompatActivity {
         // Sets the Toolbar to act as the ActionBar for this Activity window.
         // Make sure the toolbar exists in the activity and is not null
         setSupportActionBar(toolbar);
+    }
+
+    private void onPickDate() {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
     @Override
